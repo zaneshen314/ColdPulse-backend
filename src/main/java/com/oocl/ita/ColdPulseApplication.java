48b943a6db4cf7d1b164b00d98fe7daa1286743b
HEDO <hedo@oocl.com>
2024-12-06 16:59:51 +0800

    chore: rename project

--- src/main/java/com/oocl/ita/ColdPulseApplication.java
+++ src/main/java/com/oocl/ita/ColdPulseApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ColdPluseApplication {
+public class ColdPulseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ColdPluseApplication.class, args);
+        SpringApplication.run(ColdPulseApplication.class, args);
     }
 }
